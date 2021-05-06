c7d23d77a04f73c0509b57d3dd4a8d685071386e
Sara Jonsson <zara.ztrom@gmail.com>
2021-05-06 09:59:04 +0200

    started working on header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <header>
+      <h1> PensioNear </h1>
+
+      <div> Help </div>
+      <div> Profile </div>
+
+    </header>
+    <!--div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div-->
     <router-view />
   </div>
 </template>
@@ -13,8 +20,25 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
+  padding: 0;
+  margin: 0;
+  
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  background-color: #D5E7AF;
+  width: 100%;
+  min-height: 1em;
+  margin: 0;
+  top: 0;
+  padding-top: 0.1em;
+  padding-left:2em;
 }
 
 #nav {
