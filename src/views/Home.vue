d7168b42e7ca824c05b4cc9eb6b44241631623c5
Sara Jonsson <zara.ztrom@gmail.com>
2021-05-06 16:42:45 +0200

    added an indexpage with routing, need to add responsiveness

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home wrapper">
     <Assistant name="Bob"/> 
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="wrapper box_container">
+       <router-link to="/messages"><div class="btn btn_square"> <p class="btn_text">Messages</p> </div></router-link> 
+       <router-link to="/call"><div class="btn btn_square"> <p class="btn_text">Call</p> </div></router-link> 
+       <router-link to="/events"><div class="btn btn_square"> <p class="btn_text">Events</p> </div></router-link> 
+       <router-link to="/friends"><div class="btn btn_square"> <p class="btn_text">Friends</p> </div></router-link> 
+    </div>
   </div>
 </template>
 
@@ -9,14 +14,30 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import Assistant from "@/components/Assistant.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
     Assistant,
   },
 };
 </script>
+
+<style scoped lang="scss">
+  .btn_square {
+    background: rgba(213, 231, 175, 0.76);
+  }
+
+  .home .box_container {
+    grid-template-columns: repeat(4, 1fr);
+    gap: 2em;
+    margin-left: 8em;
+    margin-right: 8em;
+    margin-top: 4em;
+
+    .btn_square {
+      padding: 2em;
+    }
+  }
+</style>
---
+++ src/views/Messages.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is the message page</h1>
+  </div>
+</template>
