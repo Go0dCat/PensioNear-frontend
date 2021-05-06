34eb885181b721ad41eea033cd9c51830e3b91f9
Sara Jonsson <zara.ztrom@gmail.com>
2021-05-06 14:40:06 +0200

    added global stylesheet for scss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img class="assistant" alt="smily face" src="../assets/back.svg" />
+    
+    
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
+
+
 
 <script>
 // @ is an alias to /src
