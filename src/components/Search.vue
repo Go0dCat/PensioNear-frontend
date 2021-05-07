c2299e944347dab166e58f0fbd18787eb5f89223
id17dlm <id17dlm@cs.umu.se>
2021-05-07 14:17:16 +0200

    Fixed search box

---
+++ src/components/Search.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <input type="text" placeholder="Search here...">
+    </div>
+</template>
+<script>
+export default {
+    name: "Search",
+    props:{}
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Events.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="events">
+    <Assistant :greet="getGreeting()" :msg="getMessage()"/>
+    <Search />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Assistant from "@/components/Assistant.vue";
+import Search from '../components/Search.vue';
+
+export default {
+  name: "Help",
+  components: {
+    Assistant,
+    Search
+  },
+  data : function(){
+    return{
+      username: "Bob",
+    };
+  },
+  methods: {
+    getGreeting: function() {
+      return 'Hi ' + this.username + '!';
+    },
+    getMessage: function() {
+      return 'What kind of event are you interested in?';
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  
+</style>
