ca4742ab7015f582b5673c564863a1ae7b3a4eed
Go0dCat <zara.ztrom@gmail.com>
2021-05-25 01:11:53 +0200

    updated template for msg, altough chat history is a png

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="messages">
+  <div class="wrapper messages">
     <Assistant :greet="getGreeting()" :msg="getMessage()"/> 
     <h2>Messages </h2>
-    <p> Under construction... </p>
+  <div class="wrapper wrapper_messages">
+    <div class="msg_friendlist">
+      <CardMsg v-for="(friend,index) in friends" v-bind:key="index" :friend="friend"/>
+    </div>
+    <div class="chat">
+      <div class="wrapper chat_history" >
+        <img alt="chat box img" src="../assets/msg_box.png" />
+      </div>
+      <!--div class="chat_input">
+        <div>text field</div>
+        <div> btn</div>
+      </div-->
+    </div>
   </div>
+
+  </div>
+  
 </template>
 
 <script>
 // @ is an alias to /src
 import Assistant from "@/components/Assistant.vue";
+import CardMsg from "@/components/Card_msg.vue";
 
 export default {
   name: "Messages",
   components: {
     Assistant,
+    CardMsg
   },
   data : function(){
     return{
@@ -31,13 +48,15 @@ export default {
   computed: {
       username() {
           return this.$store.state.username;
+      },
+      friends() {
+          return this.$store.state.friends;
       }
   }
 };
 </script>
 
 <style scoped lang="scss">
-  p, h2 {
-    text-align: center;
-  }
+
+
 </style>
