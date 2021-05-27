87c30a707f503cbb057eba1a35ae61a5fe54cbcc
id17dlm <id17dlm@cs.umu.se>
2021-05-27 17:51:02 +0200

    nearby

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,15 +1,32 @@
 <template>
     <div class="wrapper search_wrapper">
-        <div>
-            <input class="search" type="text" placeholder="Search here...">
-            <button class="searchButton" type="submit"><i class="fas fa-search fa-2x"></i></button>
+        <div class="search_box">
+            <div class="search">
+                <input  type="text" id="input" placeholder="Search here...">
+                <img src="../assets/search.svg" id="input_img">
+            </div>
+            <div class="searchButton">
+                <button  type="submit">Search</button>
+            </div>
+            
+            
+        </div>
+        <div class="select_view">
+            <select name="event_cat_sort" v-bind:value="value" v-on:input="$emit('input', $event.target.value)">
+                <option value="online">Online event</option>
+                <option value="offline">Offline event</option>
+                <option value="near_by">Near by</option>
+            </select>
+            <button>Create event</button>
         </div>   
     </div>
 </template>
 <script>
 export default {
     name: "Search",
-    props:{}
+    props:['value'],
+    data: () => ({
+    }),
 };
 </script>
 <style lang="scss" scoped>
